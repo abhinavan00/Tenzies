2ee9a7d036831849bf155f758f8c295347537262
Abhinav Anand <abhinavanand072@gmail.com>
2026-06-15 11:08:47 +0530

    Header design complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    <header>
-      
-    </header>
-    </>
+    <div className='container'>
+      <header>
+        <h1>Tenzies</h1>
+        <p>
+          Roll until all dice are the same. Click each
+          die to freeze it at its current value
+          between rolls.
+        </p>
+      </header>
+    </div>
   )
 }
 
